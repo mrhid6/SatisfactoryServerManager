5c137eb2e491928d1165c7e72e9528525f4818b1
Mrhid6 <mrhid6@gmail.com>
2019-12-28 23:42:39 +0000

    version bump

--- server/server_config.js
+++ server/server_config.js
@@ -39,7 +39,7 @@ class ServerConfig extends iConfig {
 
         const defaultpasshash = CryptoJS.MD5("SSM:admin-ssm").toString();
         super.get("ssm.http_port", 3000);
-        super.set("ssm.version", "v1.0.9");
+        super.set("ssm.version", "v1.0.10");
 
         super.get("ssm.users.0.username", "admin")
         super.get("ssm.users.0.password", defaultpasshash)
