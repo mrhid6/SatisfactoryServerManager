1076afb9c0ba68c76ae87fbbfa9a0efc63efa0e2
Mrhid6 <dominic.richardson1@hotmail.co.uk>
2021-10-30 00:24:41 +0100

    chore: version bump

--- server/server_config.js
+++ server/server_config.js
@@ -63,7 +63,7 @@ class ServerConfig extends iConfig {
         const defaultpasshash = CryptoJS.MD5("SSM:admin-ssm").toString();
         super.get("ssm.setup", false)
         super.get("ssm.http_port", 3000);
-        super.set("ssm.version", `v1.0.23`);
+        super.set("ssm.version", `v1.0.24`);
 
         super.get("ssm.users.0.username", "admin")
         super.get("ssm.users.0.password", defaultpasshash)
