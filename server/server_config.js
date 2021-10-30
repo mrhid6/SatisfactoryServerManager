e8ec0f0b417b9f70ea6156c763363968f545b8c1
Mrhid6 <dominic.richardson1@hotmail.co.uk>
2021-10-30 21:55:25 +0100

    chore: bundle js & bump

--- server/server_config.js
+++ server/server_config.js
@@ -71,7 +71,7 @@ class ServerConfig extends iConfig {
 
         super.get("ssm.setup", false)
         super.get("ssm.http_port", 3000);
-        super.set("ssm.version", `v1.0.24`);
+        super.set("ssm.version", `v1.0.25`);
 
         super.get("ssm.users.0.username", "admin")
         super.get("ssm.users.0.password", defaultpasshash)
